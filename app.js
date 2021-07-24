7331e3a72a85e8132ad9c1553a80faf893f34c07
fa11en <s.gonzalez.calquin@gmail.com>
2021-07-23 20:43:40 -0400

    Modificaciones a rutas

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const app = express()
 
 app.use(express.static('public'));
 
-app.get('/hola-mundo', function (req, res) {
-    res.send('Hola mundo en su respectiva ruta')
+app.get('/generic', function (req, res) {
+    res.sendFile(__dirname + '/public/generic.html');
+})
+
+app.get('/elements', function (req, res) {
+    res.sendFile(__dirname + '/public/elements.html');
   })
 
-  app.get('*', function (req, res) {
+
+app.get('*', function (req, res) {
     res.sendFile(__dirname + '/public/404.html');
-  })
+})
  
 app.listen(8080)
