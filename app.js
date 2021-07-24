7354b6a0a4c0999df0915f3646ee0f275b574834
fa11en <s.gonzalez.calquin@gmail.com>
2021-07-24 00:11:51 -0400

    Implementacion de HBS parte 1

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const hbs = require('hbs');
+
 const app = express()
- 
+
+// Handlebar
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {}); 
+
 // Servir Contenido estatico
 
 app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render('home',{
+        nombre: 'Sebastian Gonzalez',
+        titulo: 'Curso Node'
+    });
+})
 
 app.get('/generic', function (req, res) {
     res.sendFile(__dirname + '/public/generic.html');
